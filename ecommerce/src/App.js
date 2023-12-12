ce5baf6b60493ea532c8f6932619aa0efcdf4223
FarhathH <farhathh0@gmail.com>
2023-12-12 11:51:28 +0000

    In the middle of making components of layour

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import './ecommerce.css';
+import Footer from './ecommerce.footer.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "body">
+        <div className = "header">
+          <div className = "logo">
+            <div>Insert logo here</div>
+          </div>
+          <div className = "search-bar">
+            <div>search</div>
+          </div>
+          <div className = "basket">
+            <div>basket</div>
+          </div>
+        </div>
+        <div></div>
+        <div className = "content"></div>
+        <Footer/>
+      </div>
     </div>
   );
 }
---
+++ ecommerce/src/ecommerce.footer.js
@@ -0,0 +1,26 @@
+import './ecommerce.css';
+
+function Footer(){
+    return(
+        <div className = "footer">
+        <div className = "footer-section1">
+          <div>Link 1</div>
+          <div>Link 2</div>
+          <div>Link 3</div>
+        </div>
+        <div className = "footer-section2">
+          <div>Address Line 1</div>
+          <div>Address Line 2</div>
+          <div>Email Address | Phone</div>
+          <div>Number</div>
+        </div>
+        <div className = "footer-section3">
+          <div>Link 1</div>
+          <div>Link 2</div>
+          <div>Link 3</div>
+        </div>
+      </div>
+    );
+}
+
+export default Footer;
